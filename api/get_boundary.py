c4e1a82a8e711dc8325b375b2f860e344206d769
Andrew McQueary <andrew@mcqueary.org>
2022-03-24 21:53:21 -0600

    get_boundary refactored

--- api/get_boundary.py
+++ api/get_boundary.py
@@ -5,20 +5,24 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 boundary_table = dynamodb.Table('boundary')
 
+def render(status_code, text=None, content=None):
+    if status_code==200:
+        body = json.dumps(content)
+    else:
+        body = json.dumps({"message": text})
+    return {
+        "isBase64Encoded": False,
+        "statusCode": status_code,
+        "headers": {
+            "content-type": "application/json",
+            "access-control-allow-origin": "*"
+        },
+        "body": body
+    }
+
 def lambda_handler(event, context):
     response = boundary_table.get_item(Key={'id': event['pathParameters']['boundary_id']})
     if response.get('Item') is not None:
-        return {
-            'statusCode': 200,
-            'headers': {},
-            'body': json.dumps(response['Item']),
-            'isBase64Encoded': False
-
-        }
+        return render(200, content=response['Item'])
     else :
-        return {
-            'statusCode': 404,
-            'headers': {},
-            'body': json.dumps('Not Found'),
-            'isBase64Encoded': False
-        }
+        return render(404, text="Not Found")
