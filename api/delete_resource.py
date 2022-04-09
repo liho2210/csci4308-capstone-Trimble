9d79e01f3e565d7c21cd2c8e9c56bb630d791c66
henrywangballer <hewa1459@colorado.edu>
2022-04-09 15:11:52 -0600

    update delete_resource.py

--- api/delete_resource.py
+++ api/delete_resource.py
@@ -1,9 +1,11 @@
 import json
 import boto3
+import urllib.parse
 from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb')
 resource_table = dynamodb.Table('resource')
+
 
 def render(status_code, text=None, content=None):
     return {
@@ -13,32 +15,40 @@ def render(status_code, text=None, content=None):
             "content-type": "application/json",
             "access-control-allow-origin": "*"
         },
-        "body": None if status_code == 204 else json.dumps({"message": text}) 
+        "body": None if status_code == 204 else json.dumps({"message": text})
     }
-    
+
+
 def lambda_handler(event, context):
     params = event['pathParameters']
+    boundary_id = urllib.parse.unquote_plus(params['boundary_id'])
     resource_name = params['resource_name']
-    
+    zone_id = params.get('zone_id')
+
+    response = []
+
     try:
-        #query through database with a GSI
-        data = resource_table.query (
-            IndexName='resource-index',
-            KeyConditionExpression=Key('resource').eq(resource_name)
+        # query through database with a GSI
+        data = resource_table.query(
+            IndexName='resource_name-zone_id-index',
+            KeyConditionExpression=Key('resource_name').eq(resource_name) & Key('zone_id').eq(zone_id)
         )
-        
-        if not data['Items']:
+
+        for resource in data['Items']:
+            if resource['boundary_id'] == str(boundary_id):
+                response = resource
+                break
+
+        if not response:
             return render(204)
-        
-        #delete item from DB through primary key
-        for item in data['Items']:
-            resource_table.delete_item(
-                Key={
-                    'id': item['id']
-                }
-            )
-        
+
+        resource_table.delete_item(
+            Key={
+                'id': response['id']
+            }
+        )
+
     except Exception as e:
         raise Exception(e)
-    
+
     return render(200, text='Ok')
