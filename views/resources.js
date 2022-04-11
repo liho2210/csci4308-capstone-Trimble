7a2b36f20e31b42ea39e24ddfd43437e7c864084
liho2210 <liho2210@colorado.edu>
2022-04-11 15:34:27 -0600

    create functions

--- views/resources.js
+++ views/resources.js
@@ -2,7 +2,7 @@ $(function() {
     var $resources = $('#resources');
     $.ajax({
             type: "GET",
-            url: 'https://cy08574un0.execute-api.us-east-1.amazonaws.com/dev/boundaries/229408da-73f3-403d-a5fb-f90314e9f182/zones/2/resources',
+            url: 'https://cy08574un0.execute-api.us-east-1.amazonaws.com/dev/boundaries/229408da-73f3-403d-a5fb-f90314e9f182/zones/54f6d866-5fec-4180-8ba8-853818d53a6a/resources',
             data: {},
             success: function(resources) {
               $.each(resources, function(i, resource){
@@ -20,7 +20,7 @@ $(function() {
 function create_resource(){
   $.ajax({
     type: "POST",
-    url: "https://cy08574un0.execute-api.us-east-1.amazonaws.com/dev/boundaries/229408da-73f3-403d-a5fb-f90314e9f182/zones/2/resources",
+    url: "https://cy08574un0.execute-api.us-east-1.amazonaws.com/dev/boundaries/229408da-73f3-403d-a5fb-f90314e9f182/zones/54f6d866-5fec-4180-8ba8-853818d53a6a/resources",
     data: `{
       "resource_name": "Brick",
       "resource_type": "Material",
