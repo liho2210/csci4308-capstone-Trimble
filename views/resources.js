6d9fd287f5d11c8de79ecefdb6966d9d9ff00c96
Liam Hollins <59934139+liho2210@users.noreply.github.com>
2022-04-11 14:02:36 -0600

    Update resources.js

--- views/resources.js
+++ views/resources.js
@@ -2,7 +2,7 @@ $(function() {
     var $resources = $('#resources');
     $.ajax({
             type: "GET",
-            url: 'https://cy08574un0.execute-api.us-east-1.amazonaws.com/dev/boundary',
+            url: 'https://cy08574un0.execute-api.us-east-1.amazonaws.com/dev/boundaries/229408da-73f3-403d-a5fb-f90314e9f182/zones/2/resources',
             data: {},
             success: function(resources) {
               $.each(resources, function(i, resource){
