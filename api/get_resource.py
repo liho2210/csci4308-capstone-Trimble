6922d5fb8d1fd6763ac611c1231623431dc8c4da
henrywangballer <hewa1459@colorado.edu>
2022-04-11 19:17:47 -0600

    updated get_resource.py

--- api/get_resource.py
+++ api/get_resource.py
@@ -22,24 +22,41 @@ def render(status_code, text=None, content=None):
 def lambda_handler(event, context):
     params = event['pathParameters']
     boundary_id = urllib.parse.unquote_plus(params['boundary_id'])
-    resource_name = params['resource_name']
+    resource_name = params.get('resource_name')
     zone_id = params.get('zone_id')
 
     response = []
 
     try:
-        data = resource_table.query(
-            IndexName='resource_name-zone_id-index',
-            KeyConditionExpression=Key('resource_name').eq(resource_name) & Key('zone_id').eq(zone_id)
-        )
+        if resource_name:
+            data = resource_table.query(
+                IndexName='resource_name-zone_id-index',
+                KeyConditionExpression=Key('resource_name').eq(resource_name) & Key('zone_id').eq(zone_id)
+            )
 
-        for resource in data['Items']:
-            if resource['boundary_id'] == str(boundary_id):
-                response = resource
-                break
+            for resource in data['Items']:
+                if resource['boundary_id'] == str(boundary_id):
+                    response = resource
+                    break
+
+        else:
+            data = resource_table.query(
+                IndexName='boundary_id-zone_id-index',
+                KeyConditionExpression=Key('boundary_id').eq(boundary_id) & Key('zone_id').eq(zone_id)
+            )
+            for r in data['Items']:
+                d = {
+                    'resource_name': r['resource_name'],
+                    'resource_type': r['resource_type'],
+                    'resource_status': r['resource_status'],
+                    'amount': r['amount'],
+                    'coordinates': json.loads(r['coordinates']),  # convert string list to list
+                    'description': r['description'],
+                }
+                response.append(d)
 
         if not response:
-            return render(500, text='Resource not found')
+            return render(500, text='No resource found')
 
     except Exception as e:
         raise Exception(e)
