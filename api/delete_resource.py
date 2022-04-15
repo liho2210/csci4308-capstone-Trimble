d1033b40e91fa068c05c24c8229ddf978b2e8f94
henrywangballer <hewa1459@colorado.edu>
2022-04-14 18:37:12 -0600

    update delete_resource.py

--- api/delete_resource.py
+++ api/delete_resource.py
@@ -5,6 +5,7 @@ from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb')
 resource_table = dynamodb.Table('resource')
+boundary_table = dynamodb.Table('boundary')
 
 
 def render(status_code, text=None, content=None):
@@ -28,6 +29,14 @@ def lambda_handler(event, context):
     response = []
 
     try:
+        boundary_data = boundary_table.get_item(Key={'id': boundary_id})
+
+        if 'Item' in boundary_data:
+            boundary_data = boundary_data['Item']
+            boundary_name = boundary_data.get('name', '')
+        else:
+            return render(400, 'Boundary does not exist')
+
         # query through database with a GSI
         data = resource_table.query(
             IndexName='resource_name-zone_id-index',
@@ -35,7 +44,7 @@ def lambda_handler(event, context):
         )
 
         for resource in data['Items']:
-            if resource['boundary_id'] == str(boundary_id):
+            if resource['boundary_name'] == str(boundary_name):
                 response = resource
                 break
 
