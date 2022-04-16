1929e1959ccb9180a2a1d334fd1a758173e90596
liho2210 <liho2210@colorado.edu>
2022-04-15 18:18:22 -0600

    create func parameters

--- views/zones.js
+++ views/zones.js
@@ -18,7 +18,7 @@ $(function() {
   console.log(localStorage.getItem("bound_poly"));
 })
 
-function create_zone(zone_id, polygon, description){
+function create_zone(){
 $.ajax({
   type: "POST",
   url: "https://cy08574un0.execute-api.us-east-1.amazonaws.com/dev/boundaries/"+localStorage.getItem("boundary")+"/zones",
@@ -30,26 +30,7 @@ $.ajax({
   data: `{
     "zone_id": "5",
     "polygon": [
-      [
-        -105.268,
-        40.006
-      ],
-      [
-        -105.268,
-        40.005
-      ],
-      [
-        -105.266,
-        40.005
-      ],
-      [
-        -105.266,
-        40.006
-      ],
-      [
-        -105.268,
-        40.006
-      ]
+      [40.0102, -105.2714],[40.0068, -105.2714],[40.0072, -105.2629]
     ],
     "description": "These coordinates roughly outline Farrand Field"
   }`,
