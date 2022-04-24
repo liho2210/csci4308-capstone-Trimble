4cdf354d8227f3805fddd05a2cb13eb4335e8235
Andrew McQueary <andrew@mcqueary.org>
2022-04-24 01:43:33 -0600

    get_events working as intended

--- api/get_events.py
+++ api/get_events.py
@@ -5,6 +5,7 @@ from boto3.dynamodb.conditions import Key
 dynamodb = boto3.resource('dynamodb')
 event_table = dynamodb.Table('event')
 boundary_table = dynamodb.Table('boundary')
+zone_table = dynamodb.Table('zone')
 
 def render(status_code, text=None, content=None):
     if status_code==200:
@@ -24,11 +25,17 @@ def render(status_code, text=None, content=None):
 def lambda_handler(event, context):
     if event['pathParameters'] is not None:
         boundary = boundary_table.get_item(Key={'id': event['pathParameters']['boundary_id']})
+        zone_id = event['pathParameters'].get('zone_id')
         if boundary.get('Item') is not None:
             boundary_name = boundary['Item'].get('name','')
-            k = Key('boundary_name').eq(boundary_name)
+            if 'zone_id' in event['pathParameters']:
+                if zone_id=="":
+                    return render(400,text="Must Include zone_id")
+                k = Key('boundary_name').eq(boundary_name) & Key('zone_id').eq(zone_id)
+            else:
+                k = Key('boundary_name').eq(boundary_name)
             data = event_table.query(
-                IndexName='boundary_name-index',
+                IndexName='boundary_name-zone_id-index',
                 KeyConditionExpression=k
             )
             events = sorted(data['Items'],key=lambda event: event['time_created'],reverse=True)
