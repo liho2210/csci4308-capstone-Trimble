f1e74cd7c8c9dcebe48c7278ac0a3215836ecd52
Andrew McQueary <andrew@mcqueary.org>
2022-04-24 01:21:22 -0600

    get_events workingish

--- api/get_events.py
+++ api/get_events.py
@@ -1,9 +1,10 @@
 import json
 import boto3
-
+from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb')
 event_table = dynamodb.Table('event')
+boundary_table = dynamodb.Table('boundary')
 
 def render(status_code, text=None, content=None):
     if status_code==200:
@@ -22,10 +23,16 @@ def render(status_code, text=None, content=None):
 
 def lambda_handler(event, context):
     if event['pathParameters'] is not None:
-        response = event_table.get_item(Key={'boundary_id': event['pathParameters']['boundary_id']})
-        if response.get('Item') is not None:
-            return render(200, content=response['Item'])
-        else :
-            return render(404, text="Not Found")
+        boundary = boundary_table.get_item(Key={'id': event['pathParameters']['boundary_id']})
+        if boundary.get('Item') is not None:
+            boundary_name = boundary['Item'].get('name','')
+            k = Key('boundary_name').eq(boundary_name)
+            data = event_table.query(
+                IndexName='boundary_name-index',
+                KeyConditionExpression=k
+            )
+            events = sorted(data['Items'],key=lambda event: event['time_created'],reverse=True)
+            return render(200,content=events)
+        return render(404, text="Boundary Not Found")
     else:
         return render(400, text="Must include a boundary_id")
